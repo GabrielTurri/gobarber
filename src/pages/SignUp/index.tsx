0e3388079f6f40f52066ac79044ab6fab0b20af7
Gabriel Turri <ga-briel.t.a@hotmail.com>
2020-12-03 15:06:40 -0300

    setting SignUp page validation

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiUser, FiMail, FiLock, FiArrowLeft } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
+import { abort } from 'process';
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
@@ -10,9 +12,23 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: Record<string, unknown>): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: Record<string, unknown>) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('E-mail obrigatório'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
   return (
     <Container>
       <Background />
@@ -37,7 +53,7 @@ const SignUp: React.FC = () => {
 
         <a href="login">
           <FiArrowLeft />
-          Voltra para logon
+          Voltar para logon
         </a>
       </Content>
     </Container>
