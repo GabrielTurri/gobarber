db5c37097e639508ff4beee43d0e94b6d7b2e0eb
Gabriel Turri <ga-briel.t.a@hotmail.com>
2020-12-24 14:13:35 -0300

    Add Button & Input components

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => (
   <Container>
     <Image source={logoImg} />
 
     <Title>Faça seu logon</Title>
+
+    <Input name="email" icon="mail" placeholder="E-mail" />
+
+    <Input name="password" icon="lock" placeholder="Senha" />
+
+    <Button onPress={() => { console.log('button pressed'); }}>Entrar</Button>
   </Container>
 );
 
 export default SignIn;
-// adding images and fonts
+// adding images and external fonts
