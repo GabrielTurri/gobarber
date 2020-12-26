c0e2edcc052348442f2781fda469f9e631e59f0e
Gabriel Turri <ga-briel.t.a@hotmail.com>
2020-12-25 23:40:34 -0300

    SignIn page style finished

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+  Image, View, KeyboardAvoidingView, ScrollView, Platform,
+} from 'react-native';
 
 import Icon from 'react-native-vector-icons/Feather';
 
@@ -19,22 +21,35 @@ import {
 
 const SignIn: React.FC = () => (
   <>
-    <Container>
-      <Image source={logoImg} />
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+    >
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Image source={logoImg} />
 
-      <Title>Faça seu logon</Title>
+          <View>
+            <Title>Faça seu logon</Title>
+          </View>
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="email" icon="mail" placeholder="E-mail" />
 
-      <Input name="password" icon="lock" placeholder="Senha" />
+          <Input name="password" icon="lock" placeholder="Senha" />
 
-      <Button onPress={() => { console.log('button pressed'); }}>Entrar</Button>
+          <Button onPress={() => { console.log('button pressed'); }}>Entrar</Button>
 
-      <ForgotPassword onPress={() => { console.log('button pressed'); }}>
-        <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
-      </ForgotPassword>
+          <ForgotPassword onPress={() => { console.log('button pressed'); }}>
+            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+          </ForgotPassword>
 
-    </Container>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
 
     <CreateAccountButton onPress={() => { console.log('button pressed'); }}>
       <Icon name="log-in" size={20} color="#ff9000" />
