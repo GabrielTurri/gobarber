d197ce8aaa98cdfda94a9123014a52c1d4e5125c
Gabriel Turri <ga-briel.t.a@hotmail.com>
2021-01-06 16:18:40 -0300

    Add SignIn and SignOut functions

--- mobile/src/hooks/auth.tsx
+++ mobile/src/hooks/auth.tsx
@@ -16,6 +16,7 @@ interface SignInCredentials {
 
 interface AuthContextData {
   user: Record<string, unknown>;
+  loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -24,6 +25,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStoragedData(): Promise<void> {
@@ -35,6 +37,8 @@ const AuthProvider: React.FC = ({ children }) => {
       if (token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
+
+      setLoading(false);
     }
     loadStoragedData();
   });
@@ -59,7 +63,10 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{
+      user: data.user, loading, signIn, signOut,
+    }}
+    >
       {children}
     </AuthContext.Provider>
   );
